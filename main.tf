b86062c403243d31b2d3fc0bddf432935a26a93a
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2026-07-16 13:40:04 +0530

    Module block added

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "aws_instance" "web" {
+module "ec2" {
+  source = "./modules/ec2"
   ami           = var.ami # Amazon Linux 2023 AMI (us-east-1)
   instance_type = var.instance_type              # Free-tier eligible
 
