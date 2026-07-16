1e6c841584bc4a2b3b8b01ee4dd6f6caa8644de1
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2026-07-16 14:34:32 +0530

    Checked values and noticed format is not ok.

--- main.tf
+++ main.tf
@@ -3,7 +3,5 @@ module "ec2" {
   ami           = var.ami # Amazon Linux 2023 AMI (us-east-1)
   instance_type = var.instance_type              # Free-tier eligible
 
-  tags = {
-    Name = var.instance_name
-  }
+
 }
