64a0db05506830ca67b60bd5deddb950cd8aa5db
Pankaj Patil <97191429+Pankajp1997@users.noreply.github.com>
2026-07-16 14:37:50 +0530

    test5

--- main.tf
+++ main.tf
@@ -2,6 +2,4 @@ module "ec2" {
   source = "./modules/ec2"
   ami           = var.ami # Amazon Linux 2023 AMI (us-east-1)
   instance_type = var.instance_type              # Free-tier eligible
-
-
 }
